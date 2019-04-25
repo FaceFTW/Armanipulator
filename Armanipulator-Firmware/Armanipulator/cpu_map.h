be630de7ce940fdf71b4dde5ca8b73b52304fcd0
FaceFTW <FaceFTW@outlook.com>
2019-04-25 01:51:59 -0400

    Began Firmware Work
    Created Project in Atmel Studio 7
    -Added cpu_map.h to define pins

---
+++ Armanipulator-Firmware/Armanipulator/cpu_map.h
@@ -0,0 +1,18 @@
+/*
+ * cpu_map.h
+ *
+ * Created: 4/24/2019 9:38:26 PM
+ *  Author: Alex Westerman
+ *  Used to define pins in relation to the stepper motor drivers
+ */ 
+
+
+#ifndef CPU_MAP
+#define CPU_MAP
+
+//Define Wrist Rotation Pins
+
+
+
+
+#endif
