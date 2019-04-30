c58bd013bd0e8941b8a899e3660571917b8ba405
FaceFTW <FaceFTW@outlook.com>
2019-04-30 08:54:07 -0400

    Some cleanup
    -Removed files related to the Zeus IDE
    -formatting for the parser header

--- Armanipulator_Firmware/parser.h
+++ Armanipulator_Firmware/parser.h
@@ -4,27 +4,26 @@
  * Prototype header for the parser.c file
  * Created: 4/26/2019 12:32:51 AM
  *  Author: Alex Westerman
- */ 
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef enum operation{ROTATE, GRAB, EXTEND, ERROR};
+typedef enum operation {
+	ROTATE, GRAB, EXTEND, ERROR
+};
 
-typedef struct Command{
+typedef struct Command {
 	operation op;
 	int value;
 };
-	
-	
 
-	/**
-	* \fn
-	* 
-	*
-	*/
+/**
+ * \fn
+ *
+ *
+ */
 static struct Command parse(char*);
+static void determineExec(struct Command);
 
-	
-	
