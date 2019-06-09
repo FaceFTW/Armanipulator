6fb73d62cd2a94817c5aa58bde0e7a3cb15f16db
FaceFTW <FaceFTW@outlook.com>
2019-06-08 21:45:33 -0400

    Reimplemented Client Project
    Began GUI design. Expect updates to this soon

---
+++ ArmanipulatorClient/src/main/java/MainWindow.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import com.fazecast.jSerialComm.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindow {
+    private JLabel comPortLabel;
+    private JComboBox comPortList;
+    private JButton refreshCOMPortButton;
+
+    //Utility Vars
+    SerialPort[] comPorts;
+    SerialPort currentPort;
+
+
+    public MainWindow() {
+        refreshCOMPortButton.addActionListener(new ActionListener() {           //Implementing refresh
+            public void actionPerformed(ActionEvent e) {
+                comPorts = SerialPort.getCommPorts();                           //Do the Refresh
+                //Update the combobox
+                comPortList.removeAllItems();
+                for(SerialPort port : comPorts){
+                    comPortList.addItem(port.getPortDescription());
+                }
+            }
+        });
+
+        
+
+    }
+}
